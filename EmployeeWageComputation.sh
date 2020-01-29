eb0f8e42d61a4beccaf17fea55f90f08941cc924
Pritamsuryawanshi <pritam.surya110@gmail.com>
2020-01-29 18:41:43 +0530

    ADDED: Stored day and daily wage with total wage using dictionary

--- EmployeeWageComputation.sh
+++ EmployeeWageComputation.sh
@@ -1,7 +1,7 @@
-#!/bin/bash -x
+#!/bin/bash 
 
 echo "Welcome To Employee Wage Computations"
-
+declare -A employeeDailyWage
 #CONSTANTS
 IS_PART_TIME=1;
 IS_FULL_TIME=2;
@@ -15,13 +15,13 @@ totalWorkingDays=0;
 
 function getWorkingHours() {
 	case $1 in
-   	$IS_FULL_TIME )
+   	$IS_FULL_TIME)
       	employeeHours=8
          ;;
-      $IS_PART_TIME )
+      $IS_PART_TIME)
          employeeHours=4
          ;;
-      * )
+      *)
          employeeHours=0
          ;;
 	esac
@@ -30,17 +30,18 @@ function getWorkingHours() {
 
 function calculateDailyWage() {
 	local workHours=$1
-	wage=$((workHours * EMPLOYEE_RATE_PER_HOUR))
+	wage=$((workHours*EMPLOYEE_RATE_PER_HOUR))
 	echo $wage
 }
 
-while (( $totalEmployeeHours < $MAX_HOURS_IN_MONTH && $totalWorkingDays < $NUMBER_OF_WORKING_DAYS )) 
+while (( $totalEmployeeHours < $MAX_HOURS_IN_MONTH && $totalWorkingDays < $NUMBER_OF_WORKING_DAYS ))
 do
 	((totalWorkingDays++))
 	workHours="$( getWorkingHours $((RANDOM%3)) )"
 	totalEmployeeHours=$(($totalEmployeeHours + $workHours))
-	employeeDailyWage[$totalWorkingDays]="$( calculateDailyWage $workHours )"
+	employeeDailyWage[Day" $totalWorkingDays"]="$( calculateDailyWage $workHours )"
 done
 
 totalSalary="$( calculateDailyWage $totalEmployeeHours )"
 echo "Daily Wage " ${employeeDailyWage[@]}
+echo "All Keys " ${!employeeDailyWage[@]}
