17ad749b35b408a855cccb0b39e35db775f4c0b4
Pritamsuryawanshi <pritam.surya110@gmail.com>
2020-01-29 18:07:08 +0530

    ADDED: Stored the employee's daily wage with total wage

--- EmployeeWageComputation.sh
+++ EmployeeWageComputation.sh
@@ -15,24 +15,32 @@ totalWorkingDays=0;
 
 function getWorkingHours() {
 	case $1 in
-   	$IS_FULL_TIME)
+   	$IS_FULL_TIME )
       	employeeHours=8
          ;;
-      $IS_PART_TIME)
+      $IS_PART_TIME )
          employeeHours=4
          ;;
-      *)
+      * )
          employeeHours=0
          ;;
 	esac
 	echo $employeeHours
 }
 
-while (( $totalEmployeeHours < $MAX_HOURS_IN_MONTH && $totalWorkingDays < $NUMBER_OF_WORKING_DAYS ))
+function calculateDailyWage() {
+	local workHours=$1
+	wage=$((workHours * EMPLOYEE_RATE_PER_HOUR))
+	echo $wage
+}
+
+while (( $totalEmployeeHours < $MAX_HOURS_IN_MONTH && $totalWorkingDays < $NUMBER_OF_WORKING_DAYS )) 
 do
 	((totalWorkingDays++))
 	workHours="$( getWorkingHours $((RANDOM%3)) )"
 	totalEmployeeHours=$(($totalEmployeeHours + $workHours))
+	employeeDailyWage[$totalWorkingDays]="$( calculateDailyWage $workHours )"
 done
 
-totalSalary=$(($totalEmployeeHours*$EMPLOYEE_RATE_PER_HOUR));
+totalSalary="$( calculateDailyWage $totalEmployeeHours )"
+echo "Daily Wage " ${employeeDailyWage[@]}
