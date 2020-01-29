a035ab83fb37478873ee9f628aace4809854a284
Pritamsuryawanshi <pritam.surya110@gmail.com>
2020-01-29 17:10:15 +0530

    ADDED: Calculating wages till a condition of 100 hours or 20 day is met

--- EmployeeWageComputation.sh
+++ EmployeeWageComputation.sh
@@ -1,30 +1,33 @@
-#!/bin/bash
+#!/bin/bash -x
 
 echo "Welcome To Employee Wage Computations"
 
 #CONSTANTS
 IS_PART_TIME=1;
 IS_FULL_TIME=2;
+MAX_HOURS_IN_MONTH=100;
 EMPLOYEE_RATE_PER_HOUR=20;
 NUMBER_OF_WORKING_DAYS=20;
 
-#VARIABLE
-totalSalary=0;
+#VARIABLES
+totalEmployeeHours=0;
+totalWorkingDays=0;
 
-for ((day=1; day<=$NUMBER_OF_WORKING_DAYS; day++))
+while (( $totalEmployeeHours < $MAX_HOURS_IN_MONTH && $totalWorkingDays< $NUMBER_OF_WORKING_DAYS ))
 do
+	((totalWorkingDays++))
 	employeeCheck=$((RANDOM%3));
 	case $employeeCheck in
-		$IS_FULL_TIME)
+		$IS_FULL_TIME )
 			employeeHours=8
 			;;
-		$IS_PART_TIME)
+		$IS_PART_TIME )
 			employeeHours=4
 			;;
-		*)
+		* )
 			employeeHours=0
 			;;
 	esac
-	salary=$((employeeHours*EMPLOYEE_RATE_PER_HOUR))
-	totalSalary=$(($totalSalary + $salary));
+	totalEmployeeHours=$(($totalEmployeeHours + $employeeHours))
 done
+	totalSalary=$(($totalEmployeeHours*$EMPLOYEE_RATE_PER_HOUR));
