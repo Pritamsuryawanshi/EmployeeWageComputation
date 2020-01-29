ff8f7ff15c3be58a0eab1f87e706ae6c37a4293e
Pritamsuryawanshi <pritam.surya110@gmail.com>
2020-01-29 16:25:39 +0530

    ADDED: Calculated employee's wage as per their shifts using switch case

--- EmployeeWageComputation.sh
+++ EmployeeWageComputation.sh
@@ -2,23 +2,24 @@
 
 echo "Welcome To Employee Wage Computations"
 
-
-#CONSTANT
+#CONSTANTS
 IS_PART_TIME=1;
 IS_FULL_TIME=2;
 EMPLOYEE_RATE_PER_HOUR=20;
 
 #VARIABLE
-randomCheck=$((RANDOM%3));
+employeeCheck=$((RANDOM%3));
 
-if (( $IS_FULL_TIME==$randomCheck ))
-then
-	employeeHours=8;
-elif (( $IS_PART_TIME==$randomCheck ))
-then
-	employeeHours=4;
-else
-	employeeHours=0;
-fi
+case $employeeCheck in
+	$IS_FULL_TIME)
+		employeeHours=8
+		;;
+	$IS_PART_TIME)
+		employeeHours=4
+		;;
+	*)
+		employeeHours=0
+		;;
+esac
 
 salary=$((employeeHours*EMPLOYEE_RATE_PER_HOUR));
