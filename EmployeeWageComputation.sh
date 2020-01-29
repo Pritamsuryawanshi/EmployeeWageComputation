b9c70aed74257e8a28919382f57fb473d2874377
Pritamsuryawanshi <pritam.surya110@gmail.com>
2020-01-29 15:50:00 +0530

    ADDED: Calculated daily employee wage

--- EmployeeWageComputation.sh
+++ EmployeeWageComputation.sh
@@ -4,13 +4,15 @@ echo "Welcome To Employee Wage Computations"
 
 #CONSTANT
 IS_PRESENT=1;
+EMP_RATE_PER_HR=20;
+EMP_HRS=8;
 
 #VARIABLE
 randomCheck=$((RANDOM%2));
 
 if (( $IS_PRESENT==$randomCheck ))
 then
-	echo "Employee is Present" 
+	salary=$(($EMP_HRS*$EMP_RATE_PER_HR));
 else
-	echo "Employee is Absent"
+	salary=0;
 fi
